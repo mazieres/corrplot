63f113cd9e0a5a4bc2c8901a3b63a8075cf8e5f6
Antoine Mazières <antoine.mazieres@gmail.com>
2014-04-23 17:31:27 +0200

    mycorrplot

--- corrplot.py
+++ corrplot.py
@@ -1,94 +1,60 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import pandas as pd
 from matplotlib.patches import Circle
-import string
+from scipy.stats import pearsonr
+from itertools import combinations
 
-def corrplot(data, pvalues, labels):
-	"""Creates a correlation plot of the passed data.
-
-	The function returns the plot which can then be shown with
-	plot.show(), saved to a file with plot.savefig(), or manipulated
-	in any other standard matplotlib way.
-
-	data is the correlation matrix, a 2-D numpy array containing
-	the pairwise correlations between variables;
-
-	pvalues is a matrix containing the pvalue for each corresponding
-	correlation value; if none it is assumed to be the zero matrix
-
-	labels is an array containing the variable names
+def myCorrPlot(df):
 	"""
+	Correlation plot ( ~ corrplot with R)
 
+	Takes a Pandas DataFrame as input and returns
+	the plot which can then be shown with
+    plot.show(), saved to a file with plot.savefig(), or
+    manipulated in any other standard matplotlib way.
+
+	Forked from https://github.com/louridas/corrplot
+	"""
 	plt.figure(1)
-
-	column_labels = labels
-	row_labels = labels
-	
 	ax = plt.subplot(1, 1, 1, aspect='equal')
-
-	width, height = data.shape
-	num_cols, num_rows = width, height
-
-	if pvalues is None:
-		pvalues = np.zeros([num_rows, num_cols])
-		
-	shrink = 0.9
-
 	poscm = cm.get_cmap('Blues')
-	negcm = cm.get_cmap('Oranges')
+	negcm = cm.get_cmap('Reds')
+	labels = df.columns
 
-	for x in xrange(width):
-		for y in xrange(height):
-			d = data[x, y]
-			c = pvalues[x, y]
-			rotate = -45 if d > 0 else +45
-			clrmap = poscm if d >= 0 else negcm
-			d_abs = np.abs(d)
-			ellipse = Circle((x, y), radius= 0.4 * shrink)
-			ellipse.set_edgecolor('black')
-			ellipse.set_facecolor(clrmap(d_abs))
-			if c > 0.05:
-				ellipse.set_linestyle('dotted')
-			ax.add_artist(ellipse)
+	for pair in combinations(labels, 2):
+		corr = pearsonr(df[pair[0]].values, df[pair[1]].values)[0]
+		clrmap = poscm if corr >= 0 else negcm
+		circle = Circle((labels.get_loc(pair[0]),labels.get_loc(pair[1])), radius = 0.4)
+		circle.set_edgecolor('black')
+		circle.set_facecolor(clrmap(np.abs(corr)))
+		mirrorCircle = Circle((labels.get_loc(pair[1]),labels.get_loc(pair[0])), radius = 0.4)
+		mirrorCircle.set_edgecolor('black')
+		mirrorCircle.set_facecolor(clrmap(np.abs(corr)))
+		ax.add_artist(circle)
+		ax.add_artist(mirrorCircle)
 
-	ax.set_xlim(-1, num_cols)
-	ax.set_ylim(-1, num_rows)
+	ax.set_xlim(-1, len(labels))
+	ax.set_ylim(-1, len(labels))
 		
 	ax.xaxis.tick_top()
-	xtickslocs = np.arange(len(row_labels))
+	xtickslocs = np.arange(len(labels))
 	ax.set_xticks(xtickslocs)
-	ax.set_xticklabels(row_labels, rotation=30, fontsize='small', ha='left')
+	ax.set_xticklabels(labels, rotation=30, fontsize='small', ha='left')
 
 	ax.invert_yaxis()
-	ytickslocs = np.arange(len(row_labels))
+	ytickslocs = np.arange(len(labels))
 	ax.set_yticks(ytickslocs)
-	ax.set_yticklabels(column_labels, fontsize='small')
-
+	ax.set_yticklabels(labels, fontsize='small')
+	
 	return plt
 
-if __name__ == "__main__":
-	
-	num_rows = 20
-	num_cols = num_rows
-
-	min_length = 10
-	max_length = 20
-
-	alnums = list(string.ascii_uppercase + string.digits)
-	labels = [''.join(np.random.choice(alnums,
-									   np.random.randint(min_length,
-														 max_length)))
-			  for y in np.arange(num_rows)]
-	
-   
-	data = np.random.random([num_rows, num_cols])
-
-	data[np.random.choice(num_rows, num_rows / 2), :] *= -1
-
-	np.fill_diagonal(data, 1)
-
-	data_symm = (data + data.T) / 2
-
-	plot = corrplot(data_symm, None, labels)
-	plot.show()
+if __name__ == '__main__':
+	pass
+	# # An example with the IRIS dataset
+	# from sklearn import datasets
+	# iris = datasets.load_iris()
+	# df = pd.DataFrame(iris.data, columns=iris.feature_names)
+	# plot = myCorrPlot(df)
+	# plot.show()
